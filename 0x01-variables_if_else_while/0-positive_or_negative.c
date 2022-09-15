e94d24a2341250728f93eaa2830c8b2b2ddf14c0
lavendahkinyua <you@example.com>
2022-09-15 12:52:54 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time>
+#include <time.h>
 /**
  *main - Entry point
  *
@@ -14,10 +14,10 @@ int main(){
   printf("%d is positive\n", n);
   }
  else if (n = 0){
- printf("%d is zero\n", n)
+ printf("%d is zero\n", n);
  }
  else {
- printf("%d is negative\n", n)
+ printf("%d is negative\n", n);
  }
  return 0;
 }
