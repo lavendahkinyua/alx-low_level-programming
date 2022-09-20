2a84579151c831eb162d2925edb830bdba705601
lavendahkinyua <you@example.com>
2022-09-20 04:22:35 -0700

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *reset_to_98 - takes a pointer to an int
  *and updates the value it points to to 98
@@ -7,5 +8,5 @@
  */
 void reset_to_98(int *n)
 {
-*n = 98;
+*n = 98 ;
 }
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif /* _MAIN_H_ */
