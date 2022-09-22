1569e8175b9c1c078d82fefdff33861caac6a268
lavendahkinyua <you@example.com>
2022-09-22 06:44:48 -0700

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
- *print_last_digit - last digit
- *@n: the int to print
- *Return: Always 0
+ *jack_bauer -prints every minute of the day
+ *Return: Always 0.
  */
-int print_last_digit(int n)
+void jack_bauer(void)
 {
-int last_digit;
-if (n < 0)
+int hrs;
+int min;
+for (hrs = 0; hrs <= 23; hrs++)
 {
-last_digit = (-1 * (n % 10));
-_putchar (last_digit + '0');
-return (last_digit);
+for (min = 0; min <= 59; min++)
+{
+_putchar (hrs / 10 + '0');
+_putchar (hrs % 10 + '0');
+_putchar (':');
+_putchar (min / 10 + '0');
+_putchar (min % 10 + '0');
+_putchar ('\n');
 }
-else
-{
-last_digit = (n % 10);
-_putchar (last_digit + '0');
-return (last_digit);
 }
 }
