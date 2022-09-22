86ab54738b111470bfc7689d6ae8ab00b42c87af
lavendahkinyua <you@example.com>
2022-09-22 07:08:57 -0700

    commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ *times_table - prints tables
+ *Return: Alway s0.
+ *
+ */
+void times_table(void)
+{
+int x, y;
+for (x = 0; x <= 9; x++)
+{
+for (y = 0; y <= 9; y++)
+{
+int multi = x * y;
+if (y == 0)
+{
+_putchar('0');
+}
+else if (multi <= 9)
+{
+_putchar (',');
+_putchar (' ');
+_putchar (' ');
+_putchar (multi + '0');
+}
+else
+{
+_putchar (',');
+_putchar (' ');
+_putchar (multi / 10 + '0');
+_putchar (multi % 10 + '0');
+}
+}
+_putchar ('\n');
+}
+}
