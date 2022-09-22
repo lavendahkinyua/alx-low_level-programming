66f988857a7fe6e2b303276943ca32ed41e374c5
lavendahkinyua <you@example.com>
2022-09-22 11:02:53 -0700

    commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,6 +13,13 @@ for (y = 0; y <= n; y++)
 {
 z = x * y;
 if (z > 99)
+_putchar(',');
+_putchar(32);
+_putchar((z / 100) + '0');
+_putchar(((z / 10) % 10) + '0');
+_putchar((z % 10) + '0');
+}
+else if (z > 9)
 {
 _putchar(',');
 _putchar(32);
