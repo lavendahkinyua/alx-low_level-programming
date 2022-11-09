25124d75bd479026bc94c00029d561f4b6935ccb
lavendahkinyua <you@example.com>
2022-11-09 02:46:27 -0800

    commit changes

--- 0-isupper.c
+++ 0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *main - Entry point
- *
- *Return: Always 0(success)
- */
+ * _isupper - function that checks for uppercase characters
+ * @c: int type number
+ * Return: 1 if uppercase, else 0
+ **/
 int _isupper(int c)
 {
-if (c >= 65 && c <= 90)
+if (c >= 'A' && c <= 'Z')
 return (1);
 else
 return (0);
