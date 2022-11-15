7cf10b96b8737d63a7604c2134ad46a4cec68093
lavendahkinyua <you@example.com>
2022-11-15 09:37:50 -0800

    task seven

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,7 +7,7 @@
 int is_palindrome(char *s)
 {
 int get_length(char *s);
-int my_pal(char *s, int l)
+int my_pal(char *s, int l);
 int length;
 length = get_length(s) - 1;
 return (my_pal(s, --length));
