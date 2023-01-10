5c82c556f34a5c90973b3e6d3299a6e1b6e8ed87
lavendahkinyua <you@example.com>
2023-01-10 00:57:08 -0800

    task 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,6 +9,7 @@ int _strlen(char *s)
 {
 	int i;
 	i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
@@ -27,7 +28,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int len, i;
 	len = 0;
+
 	while (src[len] != '\0')
+
 	{
 		len++;
 	}
@@ -52,6 +55,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
 	dog = malloc(sizeof(dog_t));
+
 	if (dog == NULL)
 		return (NULL);
 	dog->name = malloc(sizeof(char) * (len1 + 1));
